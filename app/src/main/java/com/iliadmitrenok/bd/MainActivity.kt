d97b200927b84b73c93f99a9757a86a3a39662fe
Ilia Dmitrenok <il.dmitrenok@gmail.com>
2019-09-08 20:38:55 +0900

    Added method to start map activity

--- app/src/main/java/com/iliadmitrenok/bd/MainActivity.kt
+++ app/src/main/java/com/iliadmitrenok/bd/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
+import android.content.Intent
+import android.net.Uri
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,9 +17,10 @@ class MainActivity : AppCompatActivity() {
         val fab = findViewById<FloatingActionButton>(R.id.fab_heart)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .show()
+            val gmmIntentUri = Uri.parse("https://www.google.com/maps/place/35%C2%B040'51.4%22N+139%C2%B045'57.7%22E/@35.6809351,139.7654738,19z/data=!3m1!4b1!4m6!3m5!1s0x0:0x0!7e2!8m2!3d35.6809337!4d139.7660212")
+            val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+            mapIntent.setPackage("com.google.android.apps.maps")
+            startActivity(mapIntent)
         }
     }
 }
